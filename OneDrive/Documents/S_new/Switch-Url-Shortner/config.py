85ea9b90555b6025419dfc74d565cd42162ef921
Top Lost <82250240+EthOxi@users.noreply.github.com>
2023-12-27 23:42:24 +0530

    Delete

--- OneDrive/Documents/S_new/Switch-Url-Shortner/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv("config.env", override=True)
-
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-ADMINS = (os.environ.get("ADMINS","")).split()
-DATABASE_URL = os.environ.get("DATABASE_URL")
-DATABASE_NAME = os.environ.get("DATABASE_NAME")
